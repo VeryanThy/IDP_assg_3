d0e41a343e14a88d2901aab4419d94a1ebe28547
Veryan Thyselius <veryanray@gmail.com>
2023-02-27 11:17:39 +0100

    objects add

--- IDP_assg_3/Program.cs
+++ IDP_assg_3/Program.cs
@@ -1,3 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace IDP_assg_3
+{
+    class MainClass
+    {
+        class Element
+        {
+            public string name, type;
+            public int Z;
+            public double meltPoint, boilPoint;
+        }
+        public static void Main(string[] args)
+        {
+            Element oxygen = new Element()
+            {
+                name = "Oxygen",
+                Z = 8,
+                type = "Non Metal",
+                meltPoint = 54.36,
+                boilPoint = 90.188,
+            };
+
+            Element iron = new Element()
+            {
+                name = "Iron",
+                Z = 26,
+                type = "Metal",
+                meltPoint = 1811,
+                boilPoint = 3243,
+            };
+
+            Element gold = new Element()
+            {
+                name = "Gold",
+                Z = 79,
+                type = "Metal",
+                meltPoint = 1337.33,
+                boilPoint = 3243,
+            };
+
+        }
+    }
+}
+
+
 
